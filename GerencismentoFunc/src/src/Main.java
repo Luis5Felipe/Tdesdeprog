c3e18e45030973de7c2e35c9949c0cddd8f2a3bb
Luis5Felipe <luisfelipedos29@gmail.com>
2024-11-25 20:18:17 -0300

    test torres

---
+++ GerencismentoFunc/src/src/Main.java
@@ -0,0 +1,14 @@
+package src;
+
+import view.FuncionarioView;
+import controller.FuncionarioController;
+
+public class Main {
+	public static void main(String[] args) {
+		FuncionarioController controller = new FuncionarioController();
+
+		FuncionarioView view = new FuncionarioView(controller);
+        
+		view.exibirMenu();
+	}
+}
